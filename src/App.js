2079da9637074c36defc1bcf0318d1d4cadd028a
syunatsu <myf.tyys@gmail.com>
2022-06-20 15:07:29 +0900

    react-stepup2(css)

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import { StyledComponents } from './components/StyledComponents';
+import { CssModules } from './components/CssModules';
+import { InlineStyle } from './components/InlineStyle';
+import { StyledJsx } from './StyledJsx';
+import { Emotion } from './components/Emotion';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <InlineStyle />
+      <CssModules />
+      <StyledJsx />
+      <StyledComponents />
+      <Emotion />
     </div>
   );
 }
---
+++ src/components/Emotion.jsx
@@ -0,0 +1,40 @@
+/** @jsxRuntime classic */
+/** @jsx jsx */
+import React from 'react';
+import { jsx, css } from "@emotion/react";
+import styled from '@emotion/styled';
+
+export const Emotion = () => {
+  const containerStyle = css`
+    border: solid 2px #392eff;
+    border-radius: 20px;
+    padding: 8px;
+    margin: 8px;
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+  `;
+  const titleStyle = css({
+    margin: 0,
+    color: '#3d84a8',
+  })
+  return (
+    <div css={containerStyle}>
+      <p css={titleStyle}>- Emotion -</p>
+      <Button>FIGHT!!</Button>
+    </div>
+  );
+};
+
+const Button = styled.button`
+  background-color: #abedd8;
+  border: none;
+  padding: 8px;
+  border-radius: 8px;
+  transition: all 0.3s;
+  &:hover {
+    background-color: #46cdcf;
+    color: #fff;
+    cursor: pointer;
+  }
+`
